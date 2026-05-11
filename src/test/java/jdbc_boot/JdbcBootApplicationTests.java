83fb6673382f1626d28a04b46acd4eb5636fd4ec
Andrew Buckingham <abuckingham99@gmail.com>
2026-05-10 20:10:03 -0700

    Add test

--- src/test/java/jdbc_boot/JdbcBootApplicationTests.java
+++ src/test/java/jdbc_boot/JdbcBootApplicationTests.java
@@ -1,13 +1,29 @@
 package jdbc_boot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class JdbcBootApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    public static final String QUERY = "SELECT count(*) FROM T_ACCOUNT";
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void testNumberOfAccount() {
+
+        long count = jdbcTemplate.queryForObject(QUERY, Long.class);
+
+        assertThat(count).isEqualTo(21L);
+    }
 }
